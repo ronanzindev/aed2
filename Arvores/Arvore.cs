8a556d2b38857369b130a5b72393380245685614
ronanzindev <morellianogm@gmail.com>
2024-06-17 20:24:57 -0300

    completando arvore

--- Arvores/Arvore.cs
+++ Arvores/Arvore.cs
@@ -1,3 +1,4 @@
+using static System.Console;
 namespace Arvores;
 
 class No {
@@ -26,6 +27,48 @@ class No {
       }
     }
   }
+  public void PreOrder() {
+    if(this == null) return;
+    WriteLine($"{elemento}");
+    left.PreOrder(); 
+    rigth.PreOrder(); 
+  }
+  public void InOrder() {
+    if(this == null)  return;
+    left.InOrder();
+    WriteLine($"{elemento}");
+    rigth.InOrder();
+  }
+  public void PosOrder(){
+    if(this == null) return;
+    left.PosOrder();
+    rigth.PosOrder();
+    WriteLine($"{elemento}");
+  }
+  public No econtrarMenorElemento() {
+    No aux = this;
+    while(aux != null && aux.left != null) {
+      aux = aux.left;
+    }
+    return aux;
+  }
+  public No deletar(int e) {
+    if(this == null) return null;
+    if(e < elemento) { 
+      left = left.deletar(elemento);
+    }
+    else if(e > elemento) { 
+     rigth = rigth.deletar(elemento);
+    } else {
+      if(left == null && rigth == null) return null;
+      if(left == null) return rigth;
+      if(rigth == null) return left;
+      No minimo = rigth.econtrarMenorElemento();
+      elemento = minimo.elemento;
+      rigth = rigth.deletar(minimo.elemento);
+    } 
+    return this;
+  }
 }
 class Arvore {
   private No raiz;
@@ -37,4 +80,11 @@ class Arvore {
     }
     raiz.Add(e);
   }
+  public void PreOrder() => raiz.PreOrder();
+  public void EmOrder() =>  raiz.InOrder();
+  public void PosOrder() => raiz.PosOrder();
+
+  public void deletar(int elemento) => raiz = raiz.deletar(elemento);
+  
+
 }
